b976511a2478924e6f987ed2a841ddfd479ad598
u22491504 <u22491504@tuks.co.za>
2024-11-04 21:14:42 +0200

    Add files via upload

--- System/StatsCommand.cpp
+++ System/StatsCommand.cpp
@@ -10,16 +10,11 @@ void StatsCommand::execute() {
         std::string input;
         std::getline(std::cin, input);
 
-        if (input == "all") {
-            std::cout << "City Stats:\n" << mediator->getAllStats();
-        } else if (input == "buildings") {
-            std::cout << "Buildings:\n" << mediator->buildingDetails();
-        } else if (input == "population") {
-            std::cout << "Total housing space:\t" << mediator->getTotalHousingSpace() << std::endl;
+        if (input == "buildings") {
+            mediator->printBuildings();
         } else {
-            std::cout << "Invalid stats command:\t" << input << std::endl;
+            std::cout << "unkown stats command: " + input << std::endl;
         }
-        
         
 	} else{
 		std::cout<<"mediator not available";
