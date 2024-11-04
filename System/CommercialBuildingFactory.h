064347317b69628361e5383a155d45f94d542383
Nabegh <u23661268@tuks.co.za>
2024-11-04 12:42:35 +0200

    CommercialBuilding/CommercialBuildingFactory Doxygen

--- System/CommercialBuildingFactory.h
+++ System/CommercialBuildingFactory.h
@@ -5,10 +5,20 @@
 #include "BuildingFactory.h"
 #include "Shop.h"
 #include "Office.h"
+
+/**
+ * @brief Factory class for creating commercial buildings
+ *
+ * CommercialBuildingFactory specializes BuildingFactory to create
+ * specific types of commercial buildings such as shops and offices.
+ */
 class CommercialBuildingFactory : public BuildingFactory
 {
-
 public:
+	/**
+	 * @brief Create a new commercial building
+	 * @return Pointer to newly created commercial Building instance
+	 */
 	Building *createBuilding();
 };
 
