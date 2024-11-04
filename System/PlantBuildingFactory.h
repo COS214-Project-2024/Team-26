43ecd1b12a40c28a18c4a46318978cbd573d79e7
Nabegh <u23661268@tuks.co.za>
2024-11-04 12:48:51 +0200

    doxygen

--- System/PlantBuildingFactory.h
+++ System/PlantBuildingFactory.h
@@ -7,10 +7,20 @@
 #include "Water.h"
 #include "Sewage.h"
 #include "Waste.h"
+
+/**
+ * @brief Factory class for creating utility plant buildings
+ * @details This factory class implements the Factory Method pattern to create
+ *          different types of utility plants including power plants, water treatment
+ *          facilities, sewage plants, and waste management facilities
+ */
 class PlantBuildingFactory : public BuildingFactory
 {
-
 public:
+	/**
+	 * @brief Creates a new plant building
+	 * @return Pointer to the newly created Building object
+	 */
 	Building *createBuilding();
 };
 
