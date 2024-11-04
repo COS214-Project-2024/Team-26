c589e539004047254acbcd5973f6e12bf542612e
u22491504 <u22491504@tuks.co.za>
2024-11-04 12:33:00 +0200

    cityCommand doxygen

--- System/CityCommand.h
+++ System/CityCommand.h
@@ -3,13 +3,27 @@
 
 #include "CityMediator.h"
 
+/**
+ * @class CityCommand
+ * @brief An abstract base class for commands.
+ * The CityCommand class serves as an interface for various commands. Derived classes must implement the 
+ * execute() method to define specific actions.
+ */
 class CityCommand {
 
 protected:
-	// CityMediator* mediator;
+    
+    // CityMediator* mediator;
 
 public:
-	virtual void execute() = 0;
+    /**
+     * @brief Pure virtual function to execute a command.
+     *
+     * This function will be overridden by derived classes to specify the command behavior.
+     */
+    virtual void execute() = 0;
 };
 
-#endif
+#endif 
+
+
